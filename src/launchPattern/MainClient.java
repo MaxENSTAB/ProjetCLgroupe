1d134dc6b76ca6d6bc81d403eb8ebc4f1bb207d1
MaxENSTAB <58601219+MaxENSTAB@users.noreply.github.com>
2020-03-19 14:01:42 +0100

    Add files via upload

--- src/launchPattern/MainClient.java
+++ src/launchPattern/MainClient.java
@@ -2,13 +2,22 @@ package launchPattern;
 
 import client.ClientTCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		ClientTCP myClt = new ClientTCP("localhost", 6666 );
 		
 		if ( myClt.connecterAuServeur() ) {
-			myClt.transmettreChaine("PING");  //TODO : à modifier
+			System.out.println("Rentre ton login \n");
+			BufferedReader reader =
+					new BufferedReader(new InputStreamReader(System.in));
+			String login = reader.readLine();
+			myClt.transmettreChaine(login);  //Pouloulou
+
 			myClt.deconnecterDuServeur();
 		}
 	
